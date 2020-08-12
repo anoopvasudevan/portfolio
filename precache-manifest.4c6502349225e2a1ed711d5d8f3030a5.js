24a6102d62d531f191e9fb7b81acfd3b4b152362
Anoop Vasudevan <postme.av@gmail.com>
2020-08-12 12:06:16 -0400

    Updates

---
+++ precache-manifest.4c6502349225e2a1ed711d5d8f3030a5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6685d35ed8f345556152f01dc4400b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a26855021cd0094ba227",
+    "url": "/portfolio/static/css/main.3de7adf7.chunk.css"
+  },
+  {
+    "revision": "d7a0627360b5886e2328",
+    "url": "/portfolio/static/js/2.1c46a314.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.1c46a314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26855021cd0094ba227",
+    "url": "/portfolio/static/js/main.807fb488.chunk.js"
+  },
+  {
+    "revision": "eb0aafffcb038f123bf0",
+    "url": "/portfolio/static/js/runtime-main.255623fe.js"
+  },
+  {
+    "revision": "ab84aa00ba63fca98592a04d79e86756",
+    "url": "/portfolio/static/media/api.ab84aa00.png"
+  },
+  {
+    "revision": "832fd50e6341abc1d8d5e1f2623a2d94",
+    "url": "/portfolio/static/media/burger-builder.832fd50e.png"
+  },
+  {
+    "revision": "a5a9770caa1931e4363c43200236124d",
+    "url": "/portfolio/static/media/css3.a5a9770c.png"
+  },
+  {
+    "revision": "dcda2df63925b7e9ef56b9aeec3a50f3",
+    "url": "/portfolio/static/media/framework.dcda2df6.png"
+  },
+  {
+    "revision": "798e00276220a676ceb24d088956ac2e",
+    "url": "/portfolio/static/media/html5.798e0027.png"
+  },
+  {
+    "revision": "a8f62ef58436b6d8d3e76685590e1e09",
+    "url": "/portfolio/static/media/myphoto.a8f62ef5.jpg"
+  },
+  {
+    "revision": "589749db57eac5029e8377a233f68fb3",
+    "url": "/portfolio/static/media/yelp-camp.589749db.png"
+  }
+]);
